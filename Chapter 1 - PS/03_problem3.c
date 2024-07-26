6072796f912c724d847dd17163c3df30a13b86a5
harshada mahajan <harshadamahajan51@gmail.com>
2024-07-26 19:13:41 +0530

    c basic concepts problem set

---
+++ Chapter 1 - PS/03_problem3.c
@@ -0,0 +1,11 @@
+//  Write a program to convert Celsius (Centigrade degrees temperature to Fahrenheit).
+
+#include<stdio.h>
+
+int main(){
+  float c = 37.0, f;
+
+  f = ((9.0/5.0) + 32);
+  printf("The value in Fahrenheit is %f", f);
+  return 0;
+}
