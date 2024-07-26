0cb3f7f014632a6ce37c75d0a83dc0d320275746
harshada mahajan <harshadamahajan51@gmail.com>
2024-07-26 18:49:45 +0530

    c basic concepts

---
+++ Chapter 1/02_variable_rules.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+int main(){
+    int harry;
+    int Harry; 
+    int harry_good;
+    return 0;
+}
+
+// Naming: Variable names must start with a letter or underscore, followed by letters, digits, or underscores.
+
+// Case Sensitivity: Variable names are case-sensitive (age and Age are different).
+
+// No Reserved Words: Variable names cannot be C reserved keywords (e.g., int, return).
+
+// Length: There is no strict limit on variable name length, but using descriptive names is recommended.
+
+// Initialization: Variables should be initialized before use to avoid unpredictable behavior
---
+++ Chapter 1/03_datatypes.c
@@ -0,0 +1,52 @@
+#include<stdio.h>
+
+int main(){
+    // int a;
+    // a = 1;
+    int a = 1;
+
+    // float b = 1.4;
+    float b;
+    b = 1.4;
+
+    // char c = 'a';
+    char c;
+    c = 'a';
+
+    printf("The value of a is %d\n", a);
+    printf("The value of b is %f\n", b);
+    printf("The value of c is %c\n", c);
+    return 0;
+}
+
+// 1. Basic Data Types
+// int
+// short
+// long
+// long long
+// unsigned
+// float
+// double
+// long double
+// char
+// unsigned char
+// signed char
+
+// 2. Derived Data Types
+// Array
+// Pointer
+// Structure
+// Union
+// Function
+
+// 3. Void Type
+// void
+// These are the primary data types used in C programming.
+
+
+
+
+
+
+
+
